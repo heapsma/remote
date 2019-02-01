9555fe8cb2195b350a69831680b36d0fce0f298d
heapsma <47257170+heapsma@users.noreply.github.com>
2019-02-01 16:11:46 -0500

    Update StudentArray.java

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentArray.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentArray.java
@@ -13,13 +13,14 @@ public class StudentArray
 {
     public static void main(String[] args)
     {
-        StudentDemo [] studentList = new StudentDemo[5];
+        StudentDemo [] studentList = new StudentDemo[6];
         StudentDemo student1 = new StudentDemo("1", "Jim Halpern");
         studentList[0] = student1;
         studentList[1] = new StudentDemo("2", "Michael Scott");
         studentList[2] = new StudentDemo("3", "Pam Beasley");
         studentList[3] = new StudentDemo("4", "Dwight Schrute");
         studentList[4] = new StudentDemo("5", "Toby Flendersen");
+        StudentList[5] = new StudentDemo("6", "Holly Flax");
         
         for(int i=0; i<studentList.length; i++)
         {
